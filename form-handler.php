199c45643b80f9661aa7b8c99313adb95fb9bf0d
Calvin Lowe <calvin@calvinlowe.com>
2017-08-04 17:00:49 +1000

    Added colour names and variables for form handler

--- form-handler.php
+++ form-handler.php
@@ -1,10 +1,10 @@
 <?php
   // The global $_POST variable allows you to access the data sent with the POST method by name
   // To access the data sent with the GET method, you can use $_GET
-  $name = htmlspecialchars($_POST['name']);
-  $email = htmlspecialchars($_POST['email']);
-  $subject = htmlspecialchars($_POST['subject']);
-  $message = htmlspecialchars($_POST['message']);
+  $name = htmlspecialchars($_POST['user_name']);
+  $email = htmlspecialchars($_POST['user_email']);
+  $subject = htmlspecialchars($_POST['email_subject']);
+  $message = htmlspecialchars($_POST['email_message']);
 
-  //echo  $say, ' ', $to;
+  echo $user_name $user_email $email_subject $email_message;
 ?>
