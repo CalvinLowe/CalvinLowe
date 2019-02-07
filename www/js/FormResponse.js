28b912595d48ac98567a9f93add181e98938eeae
Calvin Lowe <calvin.lowe@uqconnect.edu.au>
2019-02-07 16:11:24 +1000

    Added tube class FormResponse stub class

---
+++ www/js/FormResponse.js
@@ -0,0 +1,26 @@
+/**
+ * A class for representing a contact form response.
+ */
+class FormResponse {
+
+	/**
+	 * Construct a FormResponse object.
+	 * @param {string} response 
+	 * @param {string} message 
+	 * //@param {string} field
+	 */
+	constructor(response, message) {
+		this.response = response;
+		this.message = message;
+		// assign the HTML element to a variable
+
+		
+	}
+
+	display(response, message) {
+		// Get html element to display
+		// apply class based on success/failure response
+		// display this 
+		
+	}
+}
